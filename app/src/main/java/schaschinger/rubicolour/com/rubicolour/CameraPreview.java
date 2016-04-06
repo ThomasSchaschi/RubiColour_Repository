6b55cf7f178ab47e6615d9cc9aa1ad8cfaa53109
ThomasSchaschi <sch17042@spengergasse.at>
2016-04-06 20:13:13 +0200

    Added changes - can take pictures now through camera view!!!

--- app/src/main/java/schaschinger/rubicolour/com/rubicolour/CameraPreview.java
+++ app/src/main/java/schaschinger/rubicolour/com/rubicolour/CameraPreview.java
@@ -1,12 +1,24 @@
 package schaschinger.rubicolour.com.rubicolour;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
+
+import static android.support.v4.app.ActivityCompat.startActivity;
 
 /**
  * Created by thoma on 03/04/2016.
@@ -17,6 +29,9 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     private Camera mCamera;
 
     private final String TAG = "CameraPreview - ";
+
+
+
 
     public CameraPreview(Context context, Camera camera) {
         super(context);
@@ -29,6 +44,67 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // deprecated setting, but required on Android versions prior to 3.0
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
+
+    public void takeScreenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+
+            mCamera.takePicture(null, null, myPictureCallback_JPG);
+
+
+
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
+
+    Bitmap takenBitmap;
+    Camera.PictureCallback myPictureCallback_JPG = new Camera.PictureCallback(){
+
+    public void onPictureTaken(byte[] arg0, Camera arg1) {
+        // TODO Auto-generated method stub
+
+        takenBitmap = BitmapFactory.decodeByteArray(arg0, 0, arg0.length);
+        Log.i(TAG, "Bitmap obtained!");
+        int pixel = takenBitmap.getPixel(takenBitmap.getWidth() / 2 , takenBitmap.getHeight() / 2);
+        Log.i(TAG, "+" + pixel);
+
+
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        // image naming and path  to include sd card  appending name you choose for file
+        String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+
+        File imageFile = new File(mPath);
+
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            takenBitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Log.i(TAG, "SAVE!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }};
+
+
+
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
@@ -72,4 +148,6 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
         }
     }
+
+
 }
--- app/src/main/java/schaschinger/rubicolour/com/rubicolour/MainActivity.java
+++ app/src/main/java/schaschinger/rubicolour/com/rubicolour/MainActivity.java
@@ -3,22 +3,34 @@ package schaschinger.rubicolour.com.rubicolour;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -38,6 +50,9 @@ public class MainActivity extends Activity {
     private Button btnPrune;
     private ColorUtils colorUtils;
 
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,53 +60,38 @@ public class MainActivity extends Activity {
 
         this.colorUtils = new ColorUtils();
 
-        final Box box = new Box(this);
-        addContentView(box, new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
-
         // Create an instance of Camera
         mCamera = getCameraInstance();
 
 
         // Create our Preview view and set it as the content of our activity.
         mPreview = new CameraPreview(this, mCamera);
-        final FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
 
-        //For screenshot
-        this.btnPrune = (Button)findViewById(R.id.button_capture);
-        this.btnPrune.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Bitmap screenshot = screenShot(preview);
-                if(screenshot != null) {
-                    Log.i(TAG, "Screenshot taken successfully!");
+        final Box box = new Box(this);
+        addContentView(box, new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
 
-                    //Get Color
-                    int pixel = screenshot.getPixel(screenshot.getWidth()/2, screenshot.getHeight()/2);
+        //Check if camera is available
+        if(checkCameraHardware(this)) {
 
 
-                    Log.i("DETECTED COLOR : ", "Color : " + pixel);
-
-                    setContentView(R.layout.displayscreenshot);
-                    ImageView imageView = (ImageView)findViewById(R.id.imageView);
-
-
-                    imageView.setImageBitmap(screenshot);
-                }else{
-                    Log.e(TAG, "Screenshot == null!");
+            //For screenshot
+            this.btnPrune = (Button) findViewById(R.id.button_capture);
+            this.btnPrune.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mPreview.takeScreenshot();
                 }
-
-            }
-        });
+            });
+        }
 
     }
 
-    public Bitmap screenShot(View view) {
-        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
-                view.getHeight(), Bitmap.Config.ARGB_8888);
+    private static final int CAMERA_REQUEST = 1888; // field
 
-        return bitmap;
-    }
+
+
 
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
